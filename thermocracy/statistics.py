b793ecee389417f9cf65e8d9fc3b7d1e5b8e9e4d
Olivier Devauchelle <olivier@adour.ipgp.fr>
2022-01-12 18:16:32 +0100

    Uniform argument names in statistics

--- thermocracy/statistics.py
+++ thermocracy/statistics.py
@@ -1,29 +1,29 @@
 
-def neighbors_opinion( connectivity, state_vector ) :
+def neighbors_opinion( connectivity, state ) :
 
-    yes = ( state_vector > 0 )*1
-    no = ( state_vector < 0 )*1
+    yes = ( state > 0 )*1
+    no = ( state < 0 )*1
 
     yes_neighbors = ( connectivity + connectivity.T )*yes
     no_neighbors = ( connectivity + connectivity.T )*no
 
     return yes_neighbors, no_neighbors
 
-def number_of_neighbors( connectivity, state_vector ) :
+def number_of_neighbors( connectivity, state ) :
 
-    return ( connectivity + connectivity.T )*abs( state_vector )
+    return ( connectivity + connectivity.T )*abs( state )
 
-def number_of_like_minded_neighbors( connectivity, state_vector ) :
+def number_of_like_minded_neighbors( connectivity, state ) :
 
-    yes_neighbors, no_neighbors = neighbors_opinion( connectivity, state_vector )
+    yes_neighbors, no_neighbors = neighbors_opinion( connectivity, state )
 
-    yes = ( state_vector > 0 )*1
-    no = ( state_vector < 0 )*1
+    yes = ( state > 0 )*1
+    no = ( state < 0 )*1
 
     return yes_neighbors*yes + no_neighbors*no
 
-def herding_ratio( connectivity, state_vector ) :
-    return number_of_like_minded_neighbors( connectivity, state_vector )/number_of_neighbors( connectivity, state_vector )
+def herding_ratio( connectivity, state ) :
+    return number_of_like_minded_neighbors( connectivity, state )/number_of_neighbors( connectivity, state )
 
 def energy_density( H, connectivity, state ) :
     return H.get_energy( connectivity = connectivity, X = state )/len(state)
